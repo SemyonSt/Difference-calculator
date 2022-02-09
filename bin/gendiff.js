d85b135791b7e83ad794cd20be7d4bfead4ff6be
Семён Стульников <sema.stulnikov@mail.ru>
2022-02-09 22:42:45 +0300

    step two

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,9 +2,10 @@
 import {program} from 'commander';
 
 
-
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-v, --vers', 'output the current version');
+  .version('0.0.1', '-v, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1> <filepath2>');
 
   program.parse(process.argv);
