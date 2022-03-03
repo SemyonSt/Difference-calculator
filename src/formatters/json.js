4f3a7df5f4accd393f8e511abc585bf5af4e1d78
Семён Стульников <sema.stulnikov@mail.ru>
2022-03-03 21:46:11 +0300

    path

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,2 +1,2 @@
-const json = (data) => JSON.stringify(data, null, 2);
+const json = (data) => JSON.stringify(data);
 export default json;
--- src/genDiff.js
+++ src/genDiff.js
@@ -1,10 +1,24 @@
 import parse from './parsers.js';
 import getTree from './tree.js';
 import format from './formatters/index.js';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { readFileSync } from 'fs';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const file1 = parse(filepath1);
-  const file2 = parse(filepath2);
+  const getPath1 = getFixturePath(filepath1);
+  const getPath2 = getFixturePath(filepath2);
+
+  const file1 = parse(getPath1);
+  const file2 = parse(getPath2);
+
 
   const obj = getTree(file1, file2);
 
